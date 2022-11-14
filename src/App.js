a5e90fa25e7ec72f751b1350c03068915654e0a3
Omar <leosdesinganddevelopment@users.noreply.github.com>
2022-11-14 17:33:24 -0600

    Commit 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Omars gits based apps
+          Omars App
         </a>
       </header>
     </div>
