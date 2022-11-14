aa542e75f20fc49e1ea0707eaa655e2c017b55f2
Omar <leosdesinganddevelopment@users.noreply.github.com>
2022-11-14 17:29:10 -0600

    This is my first commit edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Omars gits based apps
         </a>
       </header>
     </div>
